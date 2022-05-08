21cf9e66b4f7976c48c23c73897e9bd2b982b519
Mauricio Valentim <62728083+MauricioSouzaJunior@users.noreply.github.com>
2022-05-08 13:15:30 -0300

    Initial commit

---
+++ app/src/main/java/com/shelldk/calculadoradegorjeta/MainActivity.java
@@ -0,0 +1,58 @@
+package com.shelldk.calculadoradegorjeta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView percentView, gorgeta, total;
+    EditText valor;
+    SeekBar percentSeek;
+    int progresso;
+    float valorCalculo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //declarando variaveis
+        percentView = findViewById(R.id.textViewPercent);
+        gorgeta = findViewById(R.id.textViewGorgeta);
+        total = findViewById(R.id.textViewTotal);
+        valor = findViewById(R.id.editTextValor);
+        percentSeek = findViewById(R.id.seekBarPercent);
+        percentView.setText("0%");
+        mudarPorcentagem();
+    }
+
+    private void mudarPorcentagem(){
+        percentSeek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progresso = percentSeek.getProgress();
+                percentView.setText(String.valueOf(progresso)+"%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                if(valor.getText().toString().trim().length() < 1){
+                    valorCalculo = 0;
+                }
+                else {
+                    valorCalculo = Float.valueOf(valor.getText().toString());
+                }
+                gorgeta.setText(String.format("R$%.2f", valorCalculo/100*progresso));
+                total.setText(String.format("R$%.2f", valorCalculo+(valorCalculo/100*progresso)));
+            }
+        });
+    }
+
+
+}
